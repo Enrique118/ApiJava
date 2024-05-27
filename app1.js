57ebe63a8c3d3b06b15db301fe248e00258e9e18
Hernan Martinez Bruno <hernan.martinez03@comunidad.unam.mx>
2024-05-26 20:57:13 -0600

    El formulario de alumno ya funciona

--- app1.js
+++ app1.js
@@ -23,37 +23,49 @@ document.addEventListener('DOMContentLoaded', () => {
                 data.forEach(profesor => {
                     const div = document.createElement('div');
                     div.textContent = `Nombre: ${profesor.nombre}, Correo: ${profesor.correo}, Teléfono: ${profesor.telefono}`;
-                    instructoresList.appendChild(div);
+                    profesoresList.appendChild(div);
                 });
             });
     }
 
-    inscripcionForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const formData = new FormData(inscripcionForm);
-        const alumnoData = {
-            nombre: formData.get('nombre'),
-            correo: formData.get('correo'),
-            telefono: formData.get('telefono'),
-            id_pago: formData.get('id_pago')
-        };
+    if (inscripcionForm) {
+        inscripcionForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            const formData = new FormData(inscripcionForm);
+            const alumnoData = {
+                Nombre: formData.get('Nombre'),
+                Correo: formData.get('Correo'),
+                idPago: formData.get('idPago'),
+                noTelefono: formData.get('noTelefono')
+            };
 
-        fetch('http://localhost:4000/alumno', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(alumnoData)
-        })
-        .then(response => response.json())
-        .then(data => {
-            alert('Alumno inscrito con éxito');
-            inscripcionForm.reset();
-        })
-        .catch(error => {
-            console.error('Error:', error);
+            console.log(alumnoData);
+
+
+            fetch('http://localhost:4000/alumno', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(alumnoData)
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error en la respuesta de la red');
+                }
+                return response.json();
+            })
+            .then(data => {
+                alert('Alumno inscrito con éxito');
+                inscripcionForm.reset();
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
         });
-    });
+    } else {
+        console.error('Formulario de inscripción no encontrado');
+    }
 
     obtenerTemarios();
     obtenerProfesores();
